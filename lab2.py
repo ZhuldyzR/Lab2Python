545f713081b7120f718cc86a3fb8ef226c35b833
ZhuldyzR <116473616+ZhuldyzR@users.noreply.github.com>
2024-01-31 22:05:59 +0600

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,91 @@
+#Exercise1
+print("Hello World")
+
+#Exersice2
+if 5 > 2:
+    print("YES")
+
+#Exercise3
+#This is a comment
+
+#Exercise4
+""""
+This is a comment
+written in 
+more than just one line
+"""
+
+#Exercise5
+carname = "Volvo"
+
+#Exercise6
+x = 50 
+
+#Exercise7
+x = 5
+y = 10
+print(x+y)
+
+#Exercise8
+x = 5
+y = 10
+z = x+y
+print(z)
+
+#Exercise9
+x, y, z = "Orange", "Banana", "Cherry"
+
+#Exercise10
+x = y = z = "Orange"
+
+#Exercise11
+def myfunc():
+    global x
+    x = "fantastic"
+
+#Exercise12
+x = 5
+print(type(x))
+int
+
+#Exercise13
+x = "Hello World"
+print(type(x))
+str
+
+#Exercise14
+x = 20.5
+print(type(x))
+float
+
+#Exercise15
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+#Exercise16
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#Exercise17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#Exercise18
+x = True
+print(type(x))
+bool
+
+#Exercise19
+x = 5
+x = float(x)
+
+#Exercise20
+x = 5.5
+x = int(x)
+
+#Exercise21
+x = 5
+x = complex(x)
